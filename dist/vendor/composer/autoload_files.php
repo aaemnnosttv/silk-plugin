490103491301ed654a88827aca572a7117147007
Evan Mattson <me@aaemnnost.tv>
2016-06-29 17:06:47 +0400

    Update dist to release: 0.9.0

--- dist/vendor/composer/autoload_files.php
+++ dist/vendor/composer/autoload_files.php
@@ -7,5 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     '7ba3c774c30c8399e359b5ff7f3b943e' => $vendorDir . '/tightenco/collect/src/Illuminate/Support/helpers.php',
-    '9482a87e9277ddb6e60ffd43b6b8c721' => $baseDir . '/src/functions.php',
+    '9482a87e9277ddb6e60ffd43b6b8c721' => $vendorDir . '/silk/silk/src/functions.php',
 );
